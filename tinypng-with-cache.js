9ce74b7178e35c199beb81351c77ea3e4d53eadd
unknown <eureka77035@gmail.com>
2024-10-18 13:17:44 +0800

    feat: 重构代码，创建 TinyPngCompressor 类，整合所有压缩相关功能

--- tinypng-with-cache.js
+++ tinypng-with-cache.js
@@ -4,53 +4,6 @@ const fs = require('fs-extra');
 const md5 = require('md5');
 const prettyBytes = require('pretty-bytes');
 
-let _createMd5FormOrigin = false
-let _minCompressPercentLimit = 0
-let _md5RecordFilePath = ''
-let _reportFilePath = ''
-let _apiKeyList = []
-let recordList = []
-let md5RecordList = []
-let intervalId = -1
-let currentFileName = ''
-
-let compressionInfo = {
-    num: 0,
-    saveSize: 0,
-    originSize: 0,
-    savePercent: 0,
-}
-
-console._log = (...params) => {
-    process.stdout.write(`\r`)
-    console.log(...params)
-}
-
-process.on('exit', () => recordResult(true))
-
-function recordResult(withLog) {
-    const record = `共压缩 ${compressionInfo.num} 个文件，压缩前 ${prettyBytes(compressionInfo.originSize)}，压缩后 ${prettyBytes(compressionInfo.originSize - compressionInfo.saveSize)}，节省 ${prettyBytes(compressionInfo.saveSize)} 空间，压缩百分比 ${((compressionInfo.saveSize / (compressionInfo.originSize || 1)) * 100).toFixed(0)}%`
-    withLog && console._log(record)
-    const _recordList = [].concat(recordList)
-    _recordList.push(record)
-    _md5RecordFilePath && fs.writeFileSync(_md5RecordFilePath, JSON.stringify(md5RecordList, null, 2))
-    _reportFilePath && fs.writeFileSync(_reportFilePath, JSON.stringify(_recordList, null, 2))
-}
-
-// 压缩进度条
-function spin() {
-    let index = 0
-    let dotList = '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏'
-    clearInterval(intervalId)
-    intervalId = setInterval(() => {
-        if (currentFileName) {
-            index++
-            process.stdout.write(`\r${dotList[index % dotList.length]} 压缩中 ${currentFileName}`)
-        }
-    }, 200)
-}
-
-// apiKey 管理
 class ApiKeyManager {
     constructor(apiKeys) {
         this.apiKeys = apiKeys;
@@ -82,130 +35,194 @@ class ApiKeyManager {
     }
 }
 
-// 压缩图片请求
-async function tinypng(file, apiKeyManager, cb) {
-    try {
-        const response = await axios({
-            method: 'POST',
-            url: 'https://api.tinypng.com/shrink',
-            data: file.contents,
-            headers: {
-                'Accept': '*/*',
-                'Cache-Control': 'no-cache',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + apiKeyManager.getToken(),
-            },
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false
-            }),
+class TinyPngCompressor {
+    constructor(options) {
+        this._createMd5FormOrigin = options.createMd5FormOrigin;
+        this._minCompressPercentLimit = options.minCompressPercentLimit;
+        this._md5RecordFilePath = options.md5RecordFilePath;
+        this._reportFilePath = options.reportFilePath;
+        this.md5RecordList = [];
+        this.recordList = [];
+        this.compressionInfo = {
+            num: 0,
+            saveSize: 0,
+            originSize: 0,
+            savePercent: 0,
+        };
+        this.currentFileName = '';
+        this.apiKeyManager = new ApiKeyManager(options.apiKeyList);
+        this.intervalId = -1;
+
+        this.initializeMd5RecordList();
+        this.spin();
+
+        // 监听进程退出事件，记录压缩结果
+        process.on('exit', () => this.recordResult(true));
+    }
+
+    // 初始化 md5 记录列表
+    initializeMd5RecordList() {
+        try {
+            this.md5RecordList = JSON.parse(fs.readFileSync(this._md5RecordFilePath) || '[]');
+        } catch (e) {
+            console.error('[error] : 读取 md5 记录文件出错了 - ', e);
+        }
+    }
+
+    // 压缩进度条
+    spin() {
+        let index = 0;
+        let dotList = '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏';
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(() => {
+            if (this.currentFileName) {
+                index++;
+                process.stdout.write(`\r${dotList[index % dotList.length]} 压缩中 ${this.currentFileName}`);
+            }
+        }, 200);
+    }
+
+    // 压缩文件
+    async compress(file) {
+        if (this._createMd5FormOrigin) {
+            this.md5RecordList.push(md5(file.contents));
+            return file;
+        }
+
+        if (this._md5RecordFilePath && this.md5RecordList.indexOf(md5(file.contents)) > -1) {
+            return file;
+        }
+
+        this.currentFileName = file.relative;
+        let prevSize = file.contents.length;
+        if (!prevSize) {
+            console.log(`${this.currentFileName} 文件信息错误，无法获取文件大小，跳过压缩`);
+            return file;
+        }
+
+        return new Promise((resolve) => {
+            this.tinypng(file, (data) => {
+                this.processCompressedData(file, data, prevSize);
+                this.recordResult();
+                resolve(file);
+            });
         });
+    }
 
-        const results = response.data;
-        if (results.output && results.output.url) {
-            try {
-                const compressedFileResponse = await axios({
-                    method: 'get',
-                    url: results.output.url,
-                    responseType: 'arraybuffer',
-                    httpsAgent: new https.Agent({
-                        rejectUnauthorized: false
-                    }),
-                });
-                cb(compressedFileResponse.data);
-            } catch (downLoadError) {
-                console._log('[error] : 文件下载错误 - ', downLoadError)
+    async tinypng(file, cb) {
+        try {
+            const response = await axios({
+                method: 'POST',
+                url: 'https://api.tinypng.com/shrink',
+                data: file.contents,
+                headers: {
+                    'Accept': '*/*',
+                    'Cache-Control': 'no-cache',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic ' + this.apiKeyManager.getToken(),
+                },
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                }),
+            });
+
+            const results = response.data;
+            // 压缩成功后下载压缩后的文件
+            if (results.output && results.output.url) {
+                try {
+                    const compressedFileResponse = await axios({
+                        method: 'get',
+                        url: results.output.url,
+                        responseType: 'arraybuffer',
+                        httpsAgent: new https.Agent({
+                            rejectUnauthorized: false
+                        }),
+                    });
+                    cb(compressedFileResponse.data);
+                } catch (downLoadError) {
+                    console.log('[error] : 文件下载错误 - ', downLoadError);
+                }
+            } else {
+                this.handleError(results.message, () => this.tinypng(file, cb), () => cb(file.contents));
+            }
+        } catch (error) {
+            this.handleError(error.response.data.message, () => this.tinypng(file, cb), () => cb(file.contents));
+        }
+    }
+
+    handleError(errorMsg, tryNextKeyCb, skipFileCb) {
+        const keyErrorList = [
+            'Credentials are invalid.',
+            'Your monthly limit has been exceeded.',
+        ];
+        if (keyErrorList.includes(errorMsg)) {
+            if (this.apiKeyManager.nextKey()) {
+                tryNextKeyCb();
+            } else {
+                clearInterval(this.intervalId);
+                console.log('所有API Key均已失效或超限，无法继续压缩。');
             }
         } else {
-            handleError(results.message, apiKeyManager, tinypng.bind(null, file, apiKeyManager, cb), () => cb(file.contents));
+            console.log('[error] : 文件不可压缩 - ', errorMsg);
+            skipFileCb();
         }
-    } catch (error) {
-        handleError(error.response.data.message, apiKeyManager, tinypng.bind(null, file, apiKeyManager, cb), () => cb(file.contents));
     }
-}
 
-// 处理错误
-async function handleError(errorMsg, apiKeyManager, tryNextKeyCb, skipFileCb) {
-    const keyErrorList = [
-        'Credentials are invalid.',
-        'Your monthly limit has been exceeded.',
-    ];
-    if (keyErrorList.includes(errorMsg)) {
-        if (apiKeyManager.nextKey()) {
-            tryNextKeyCb();
+    processCompressedData(file, data, prevSize) {
+        const compressPercent = (1 - data.length / prevSize) * 100;
+        const compressPercentStr = compressPercent.toFixed(0) + '%';
+        
+        if (compressPercent < this._minCompressPercentLimit) {
+            this.handleLowCompression(file, compressPercentStr);
         } else {
-            clearInterval(intervalId)
-            console.log('所有API Key均已失效或超限，无法继续压缩。');
+            this.handleSuccessfulCompression(file, data, prevSize, compressPercent);
         }
-    } else {
-        console.log('[error] : 文件不可压缩 - ', errorMsg);
-        skipFileCb();
+    }
+
+    handleLowCompression(file, compressPercentStr) {
+        this.md5RecordList.push(md5(file.contents));
+        console.log(`压缩比例低于安全线，保存源文件: ${file.relative} 【${compressPercentStr}】`);
+    }
+
+    handleSuccessfulCompression(file, data, prevSize, compressPercent) {
+        file.contents = data;
+        this.updateCompressionInfo(prevSize, data.length);
+        this.md5RecordList.push(md5(data));
+        const record = this.createCompressionRecord(file, prevSize, data.length, compressPercent);
+        this.recordList.push(record);
+        console.log(record);
+    }
+
+    updateCompressionInfo(prevSize, newSize) {
+        this.compressionInfo.num++;
+        this.compressionInfo.saveSize += prevSize - newSize;
+        this.compressionInfo.originSize += prevSize;
+    }
+
+    createCompressionRecord(file, prevSize, newSize, compressPercent) {
+        let record = '压缩成功  ';
+        record += `前: ${prettyBytes(prevSize)}`.padEnd(15);
+        record += `后: ${prettyBytes(newSize)}`.padEnd(15);
+        record += `压缩: ${prettyBytes(prevSize - newSize)}`.padEnd(18);
+        record += `压缩百分比: ${compressPercent.toFixed(2)}%`.padEnd(18);
+        record += `${file.relative}`;
+        return record;
+    }
+
+    recordResult(withLog = false) {
+        const record = `共压缩 ${this.compressionInfo.num} 个文件，压缩前 ${prettyBytes(this.compressionInfo.originSize)}，压缩后 ${prettyBytes(this.compressionInfo.originSize - this.compressionInfo.saveSize)}，节省 ${prettyBytes(this.compressionInfo.saveSize)} 空间，压缩百分比 ${((this.compressionInfo.saveSize / (this.compressionInfo.originSize || 1)) * 100).toFixed(0)}%`;
+        withLog && console.log(record);
+        const _recordList = [].concat(this.recordList);
+        _recordList.push(record);
+        this._md5RecordFilePath && fs.writeFileSync(this._md5RecordFilePath, JSON.stringify(this.md5RecordList, null, 2));
+        this._reportFilePath && fs.writeFileSync(this._reportFilePath, JSON.stringify(_recordList, null, 2));
+    }
+
+    finish() {
+        clearInterval(this.intervalId);
     }
 }
 
-function main({ apiKeyList = [], md5RecordFilePath, reportFilePath, minCompressPercentLimit = 0, createMd5FormOrigin = false }) {
-    const apiKeyManager = new ApiKeyManager(apiKeyList);
-    _apiKeyList = apiKeyList
-    _md5RecordFilePath = md5RecordFilePath
-    _reportFilePath = reportFilePath
-    _minCompressPercentLimit = minCompressPercentLimit
-    _createMd5FormOrigin = createMd5FormOrigin
-    
-    spin()
-    try {
-        md5RecordList = JSON.parse(fs.readFileSync(_md5RecordFilePath) || '[]')
-    } catch (e) {
-        // 处理文件读取错误
-    }
-
-    return {
-        compress: async function(file) {
-            if (_createMd5FormOrigin) {
-                md5RecordList.push(md5(file.contents))
-                return file
-            }
-
-            if (_md5RecordFilePath && md5RecordList.indexOf(md5(file.contents)) > -1) {
-                return file
-            }
-
-            currentFileName = file.relative
-            let prevSize = file.contents.length
-            if (!prevSize) {
-                console.log(`${currentFileName} 文件信息错误，无法获取文件大小，跳过压缩`)
-                return file
-            }
-
-            return new Promise((resolve) => {
-                tinypng(file, apiKeyManager, (data) => {
-                    const compressPercent = (1 - data.length / prevSize) * 100
-                    const compressPercentStr = compressPercent.toFixed(0) + '%'
-                    if (compressPercent < _minCompressPercentLimit) {
-                        md5RecordList.push(md5(file.contents))
-                        console._log(`压缩比例低于安全线，保存源文件: ${file.relative} 【${compressPercentStr}】`)
-                    } else {
-                        file.contents = data
-                        compressionInfo.num++
-                        compressionInfo.saveSize += prevSize - data.length
-                        compressionInfo.originSize += prevSize
-                        md5RecordList.push(md5(data))
-                        let record = '压缩成功  '
-                        record += `前: ${prettyBytes(prevSize)}`.padEnd(15)
-                        record += `后: ${prettyBytes(data.length)}`.padEnd(15)
-                        record += `压缩: ${prettyBytes(prevSize - data.length)}`.padEnd(18)
-                        record += `压缩百分比: ${((prevSize - data.length) / (prevSize || 1) * 100).toFixed(2)}%`.padEnd(18)
-                        record += `${file.relative} `
-                        recordList.push(record)
-                        console._log(record)
-                    }
-                    recordResult()
-                    resolve(file)
-                })
-            })
-        },
-        finish: function() {
-            clearInterval(intervalId)
-        }
-    }
-}
-
-module.exports = main;
+module.exports = function(options) {
+    return new TinyPngCompressor(options);
+};
